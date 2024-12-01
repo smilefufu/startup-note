2ad1b9d65f30edd6bf47d5fb318e6f9b492d1bd7
TopCaver <zhangbo@topcaver.com>
2024-12-01 11:33:23 +0800

    新增注册dify的流程

--- wxcloudrun/model.py
+++ wxcloudrun/model.py
@@ -13,3 +13,26 @@ class Counters(db.Model):
     count = db.Column(db.Integer, default=1)
     created_at = db.Column('createdAt', db.TIMESTAMP, nullable=False, default=datetime.now())
     updated_at = db.Column('updatedAt', db.TIMESTAMP, nullable=False, default=datetime.now())
+
+# Dify用户表
+
+class DifyUsers(db.Model):
+    __tablename__ = 'DifyUsers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    # XX_ 代表来自小栖平台的数据，包括用户名、手机号、uuid
+    xx_user_name =  db.Column(db.String(100), nullable=False)
+    xx_phone_num = db.Column(db.String(100), nullable=False)
+    xx_xiaoxi_uuid = db.Column(db.String(100), nullable=False)
+    # wx_  代表来自微信平台等数据
+    wx_openid = db.Column(db.String(100), nullable=True)
+    wx_unionid = db.Column(db.String(100), nullable=True)
+    wx_ip = db.Column(db.String(100), nullable=True)
+    wx_source = db.Column(db.String(100), nullable=True)
+    # dify_ 代表来自dify平台的数据，包括email、token、invite_url等
+    dify_email = db.Column(db.String(100), nullable=True)
+    dify_token = db.Column(db.String(100), nullable=True)
+    dify_invite_url = db.Column(db.String(500), nullable=True)
+    # 其余数据
+    created_at = db.Column('createdAt', db.TIMESTAMP, nullable=False, default=datetime.now())
+    updated_at = db.Column('updatedAt', db.TIMESTAMP, nullable=False, default=datetime.now())
--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,11 +1,18 @@
 from datetime import datetime
 import json
-from flask import render_template, request, Response
+import re
+from flask import render_template, request, Response, redirect
 from run import app
 from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
-from wxcloudrun.model import Counters
+from wxcloudrun.model import Counters, DifyUsers
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
+from wxcloudrun import db
+import requests
 
+EMAIL_REGEX = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+
+def is_valid_email(email):
+    return re.match(EMAIL_REGEX, email) is not None
 
 def jsonify(data):
     # 使用自定义的 JSON 响应函数，并确保 ensure_ascii=False
@@ -106,9 +113,23 @@ def gzh_msg():
             sp = content.split(":")
             app.logger.info("get content: %s", str(sp))
             if len(sp) == 2:
-                email = sp[-1]
-                # TODO: 注册用户
-                return reply_text(me, from_user, "注册功能还在开发中，敬请期待！")
+                email = sp[-1].strip()
+                if is_valid_email(email=email):
+                    # 注册用户
+                    invite_result = requests.get("http://dify.vongcloud.com/xinshu/api/invite",params={"email":email})
+                    if invite_result.ok:
+                        invite_json = invite_result.json()
+                        if invite_json["result"] == "success":
+                            invite_url = invite_json["invite-url"]
+                            return reply_text(me, from_user, f"请在浏览器打开以下链接，完成注册：{invite_url}，您的注册邮箱是：{email}")
+                        else:
+                            invite_error = invite_json["error"]
+                            return reply_text(me, from_user, f"遇到一点点小麻烦：{invite_error}")
+                    else:
+                        return reply_text(me, from_user, f"遇到一点点小麻烦,需要召唤管理员：{invite_result.text}")
+                else:
+                    return reply_text(me, from_user, f"邮箱格式有点点小问题，这样写-->  注册：zhangsan@example.com")
+            return reply_text(me, from_user, f"格式有点点小问题，这样写：注册：zhangsan@example.com")
     elif msg_type == 'event':
         reply = "你好，感谢您的关注！"
         return reply_text(me, from_user, reply)
@@ -120,11 +141,23 @@ def landing_page():
     phone_num = request.args.get("phone_num")
     xiaoxi_uuid = request.args.get("xiaoxi_uuid")
     app.logger.info(f"落地页面跳转：{user_name},{phone_num}, {xiaoxi_uuid}")
-    # TODO 校验是否关注了公众号
-    
-    # TODO 已关注公众号，跳转
-    
-    # TODO 未关注公众号，展示关注公众号
+    # 校验是否已有账号
+    user = DifyUsers.query.filter_by(xx_xiaoxi_uuid=xiaoxi_uuid).first()
+    if user:
+        if user.dify_email and user.dify_token:
+            # 曾经注册过dify， 直接跳转
+            return redirect("https://dify.vongcloud.com")    
+        else:
+            # 跳转过，但是未完成注册
+            pass
+    else:
+        # 新用户，需要重新引导
+        new_user = DifyUsers(
+            xx_user_name = user_name,
+            xx_phone_num = phone_num,
+            xx_xiaoxi_uuid = xiaoxi_uuid
+        )
+        db.session.add(new_user)
+        db.session.commit()
     return render_template('landing_page.html')
     
-    # return make_succ_response(data={"user_name":user_name, "phone_num":phone_num, "xiaoxi_uuid":xiaoxi_uuid})
